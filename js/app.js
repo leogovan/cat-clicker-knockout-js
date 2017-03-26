ce59491a64bcbcece00310b3ad3ee2a7d9363f5b
Leofric Govan <leogovan@Leos-MacBook-Pro-2.local>
2017-03-26 20:19:10 +0100

    Clicker with levels

---
+++ js/app.js
@@ -0,0 +1,32 @@
+var ViewModel = function() {
+	this.clickCount = ko.observable(0);
+	this.name = ko.observable("Tabby");
+	this.imgSrc = ko.observable("img/434164568_fea0ad4013_z.jpg");
+	this.imgAttribution = ko.observable("");
+	this.level = ko.computed(function() {
+		if (this.clickCount() < 10) {
+			return "Infant";
+		}
+		if (this.clickCount() < 20 ){
+			return "Adolescent";
+		}
+		if (this.clickCount() < 30 ){
+			return "Adult";
+		}
+		if (this.clickCount() < 40 ){
+			return "Old Age";
+		}
+		if (this.clickCount() < 50 ){
+			return "Last Legs";
+		}
+		else {
+			return "Dead";
+		};
+	}, this);
+
+	this.incrementCounter = function() {
+		this.clickCount(this.clickCount() + 1);
+	};
+};
+
+ko.applyBindings(new ViewModel());
