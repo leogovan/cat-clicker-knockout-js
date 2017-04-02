35ff40b9f9036760753fa6842864d8d5d5a0c354
Leofric Govan <leogovan@Leos-MacBook-Pro-2.local>
2017-04-02 17:21:47 +0100

    multiple cats, render cat names, click to change cat

--- js/app.js
+++ js/app.js
@@ -1,9 +1,42 @@
-var Cat = function() {
-	this.clickCount = ko.observable(0);
-	this.name = ko.observable("Tabby");
-	this.imgSrc = ko.observable("img/434164568_fea0ad4013_z.jpg");
-	this.imgAttribution = ko.observable("");
-	this.nickNames = ko.observableArray([{name: "Cribbinbob"},{name: "Thatcher"},{name: "Threadbats"},{name: "Redders"}]);
+var initialCats = [{
+	clickCount: 0,
+	name: "Dave",
+	imgSrc: "img/kitty-1.jpg",
+	nickNames: [{name: "Cribbinbob"},{name: "Thatcher"},{name: "Threadbats"},{name: "Redders"}]
+},
+{
+	clickCount: 0,
+	name: "Steve",
+	imgSrc: "img/kitty-2.jpg",
+	nickNames: [{name: "Nutster"}]
+},
+{
+	clickCount: 0,
+	name: "Tim",
+	imgSrc: "img/kitty-3.jpg",
+	nickNames: [{name: "Milk-fiend"}]
+},
+{
+	clickCount: 0,
+	name: "Eustace",
+	imgSrc: "img/kitty-4.jpg",
+	nickNames: [{name: "Fishypants"}]
+},
+{
+	clickCount: 0,
+	name: "Alan",
+	imgSrc: "img/kitty-5.jpg",
+	nickNames: [{name: "Gen dit"}]
+}];
+
+
+
+
+var Cat = function(data) {
+	this.clickCount = ko.observable(data.clickCount);
+	this.name = ko.observable(data.name);
+	this.imgSrc = ko.observable(data.imgSrc);
+	this.nickNames = ko.observableArray([data.nickNames.name]);
 
 	this.level = ko.computed(function() {
 		if (this.clickCount() < 10) {
@@ -31,10 +64,22 @@ var Cat = function() {
 
 
 var ViewModel = function() {
-	this.currentCat = ko.observable( new Cat() );
+	var self = this;
+
+	this.catList = ko.observableArray([]);
+
+	initialCats.forEach(function(catItem){
+		self.catList.push( new Cat(catItem) );
+	});
+
+	this.currentCat = ko.observable( this.catList()[0] );
 
 	this.incrementCounter = function() {
-		this.clickCount(this.clickCount() + 1);
+		self.currentCat().clickCount(self.currentCat().clickCount() + 1);
+	};
+
+	this.setCat = function(clickedCat) {
+		self.currentCat(clickedCat);
 	};
 };
 
